54a67d250c1cafdbe4a57282dadac05006d8a925
yash-kalathiya <kalathiyayash163@gmail.com>
2026-01-16 17:08:30 -0800

    done

--- dish-finder/backend/app/config.py
+++ dish-finder/backend/app/config.py
@@ -39,6 +39,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
         case_sensitive = False
+        extra = "ignore"  # Ignore extra env vars like VITE_*
 
 
 @lru_cache()
